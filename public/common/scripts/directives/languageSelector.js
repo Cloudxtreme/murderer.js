137d854f0777994b58a07675efb937be75f18752
frissdiegurke <frissdiegurke@hotmail.com>
2015-12-15 09:39:19 +0100

    Refactor layout with focus on navigation

--- public/common/scripts/directives/languageSelector.js
+++ public/common/scripts/directives/languageSelector.js
@@ -2,7 +2,7 @@ angular.module("common").directive("languageSelector", function (locales) {
   "use strict";
 
   return {
-    restrict: "A",
+    restrict: "C",
     templateUrl: "/templates/common/language_selector.html",
     link: function ($scope) {
       $scope.locales = locales;
