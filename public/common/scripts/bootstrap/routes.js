6353346bc4d6364023b199a413b123d04b7c3c39
frissdiegurke <frissdiegurke@hotmail.com>
2015-12-15 10:44:32 +0100

    Add footer navigation items

--- public/common/scripts/bootstrap/routes.js
+++ public/common/scripts/bootstrap/routes.js
@@ -8,13 +8,26 @@ angular.module("common").config(function ($routeProvider, $locationProvider) {
       .when("/", {
         templateUrl: "/views/common/home.html"
       })
+
+      .when("/terms", {
+        templateUrl: "/views/common/terms.html"
+      })
+      .when("/policy", {
+        templateUrl: "/views/common/policy.html"
+      })
+      .when("/disclaimer", {
+        templateUrl: "/views/common/disclaimer.html"
+      })
+
       .when("/game", {
         templateUrl: "/views/common/game.html"
       })
+
       .when("/statistics", {
         templateUrl: "/views/common/stats.html",
         resolve: {cacheInvalidation: function (stats) { stats.clearCache(); }}
       })
+
       .otherwise({
         templateUrl: "/views/common/404.html"
       });
