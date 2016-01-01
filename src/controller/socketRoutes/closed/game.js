6c782945ee9d83b4363c6fd8ceaada0bcbda7b48
frissdiegurke <frissdiegurke@hotmail.com>
2016-01-01 17:56:35 +0100

    Deactivate non-reworked query routes

--- src/controller/socketRoutes/closed/game.js
+++ src/controller/socketRoutes/closed/game.js
@@ -23,38 +23,38 @@ module.exports = function (queryRoute) {
     return gameC.qLeave(this, data);
   });
 
-  queryRoute("game:contracts", function (data, cb) { // TODO move logic into game controller, q-based
-    var scope = this;
-    var userId = scope.user._id;
-    gameM.findOne({active: true}).populate("rings.active.user").exec(function (err, game) {
-      if (err != null) {
-        return cb(err);
-      }
-      if (game == null) {
-        return cb(new Error("Game not found."));
-      }
-      if (!_.some(game.participants, function (p) { return p.equals(userId); })) {
-        return cb(new Error("Not participating."));
-      }
-      var result = {};
-      result.game = _.pick(game, ["name"]);
-      result.rings = _.map(game.rings, function (ring, index) {
-        var r = {ring: index};
-        if (ring.active.length <= 1) {
-          r.resolved = true;
-          r.lastSurvivor = ring.active.length ? _.pick(ring.active[0].user, ["_id", "username"]) : null;
-          return r;
-        }
-        var idx = _.findIndex(ring.active, function (obj) { return obj.user._id.equals(userId); });
-        if (~idx) {
-          var mission = ring.active[idx + 1 < ring.active.length ? idx + 1 : 0].user;
-          r.active = true;
-          r.token = ring.active[idx].token;
-          r.mission = _.pick(mission, ["_id", "username", "profileMessage"]);
-        }
-        return r;
-      });
-      cb(null, result);
-    });
-  });
+  //queryRoute("game:contracts", function (data, cb) { // TODO move logic into game controller, q-based
+  //  var scope = this;
+  //  var userId = scope.user._id;
+  //  gameM.findOne({active: true}).populate("rings.active.user").exec(function (err, game) {
+  //    if (err != null) {
+  //      return cb(err);
+  //    }
+  //    if (game == null) {
+  //      return cb(new Error("Game not found."));
+  //    }
+  //    if (!_.some(game.participants, function (p) { return p.equals(userId); })) {
+  //      return cb(new Error("Not participating."));
+  //    }
+  //    var result = {};
+  //    result.game = _.pick(game, ["name"]);
+  //    result.rings = _.map(game.rings, function (ring, index) {
+  //      var r = {ring: index};
+  //      if (ring.active.length <= 1) {
+  //        r.resolved = true;
+  //        r.lastSurvivor = ring.active.length ? _.pick(ring.active[0].user, ["_id", "username"]) : null;
+  //        return r;
+  //      }
+  //      var idx = _.findIndex(ring.active, function (obj) { return obj.user._id.equals(userId); });
+  //      if (~idx) {
+  //        var mission = ring.active[idx + 1 < ring.active.length ? idx + 1 : 0].user;
+  //        r.active = true;
+  //        r.token = ring.active[idx].token;
+  //        r.mission = _.pick(mission, ["_id", "username", "profileMessage"]);
+  //      }
+  //      return r;
+  //    });
+  //    cb(null, result);
+  //  });
+  //});
 };
--- src/controller/socketRoutes/common/news.js
+++ src/controller/socketRoutes/common/news.js
@@ -6,48 +6,48 @@ var gameM = require.main.require("./core/game/model");
 var newsM = require.main.require("./core/message/model");
 
 module.exports = function (queryRoute) {
-  queryRoute("news:global", function (data, cb) { // TODO move logic into controller, q-based
-    newsM.find({game: null}).populate("author game").exec(function (err, news) {
-      if (err != null) {
-        return cb(err);
-      }
-      cb(null, _.map(news, function (n) {
-        return _.extend(_.omit(n._doc, ["author", "game"]), {
-          author: _.pick(n.author._doc, ["_id", "username"]),
-          game: n.game ? _.pick(n.game._doc, ["_id", "name"]) : null
-        });
-      }));
-    });
-  });
-
-  queryRoute("news:game", function (data, cb) { // TODO move logic into controller, q-based
-    gameM.findOne({active: true}, {kills: 1}).populate("kills.murderer kills.victim").exec(function (err, game) {
-      if (err != null) {
-        return cb(err);
-      }
-      if (game == null) {
-        return cb(new Error("Game not found."));
-      }
-      newsM.find({game: game._id}).populate("author").exec(function (err, news) {
-        if (err != null) {
-          return cb(err);
-        }
-        var kills = _.sortByOrder(game.kills, ["entryDate"], ["desc"]);
-        if (typeof data === "number" && data > 0) {
-          kills = _.slice(kills, 0, data);
-        }
-        cb(null, {
-          kills: _.map(kills, function (k) {
-            return _.extend(_.omit(k._doc, ["murderer", "victim"]), {
-              murderer: k.murderer ? _.pick(k.murderer._doc, ["_id", "username"]) : null,
-              victim: _.pick(k.victim._doc, ["_id", "username"])
-            });
-          }),
-          special: _.map(news, function (n) {
-            return _.extend(_.omit(n._doc, ["author"]), {author: _.pick(n.author._doc, ["_id", "username"])});
-          })
-        });
-      });
-    });
-  });
+  //queryRoute("news:global", function (data, cb) { // TODO move logic into controller, q-based
+  //  newsM.find({game: null}).populate("author game").exec(function (err, news) {
+  //    if (err != null) {
+  //      return cb(err);
+  //    }
+  //    cb(null, _.map(news, function (n) {
+  //      return _.extend(_.omit(n._doc, ["author", "game"]), {
+  //        author: _.pick(n.author._doc, ["_id", "username"]),
+  //        game: n.game ? _.pick(n.game._doc, ["_id", "name"]) : null
+  //      });
+  //    }));
+  //  });
+  //});
+  //
+  //queryRoute("news:game", function (data, cb) { // TODO move logic into controller, q-based
+  //  gameM.findOne({active: true}, {kills: 1}).populate("kills.murderer kills.victim").exec(function (err, game) {
+  //    if (err != null) {
+  //      return cb(err);
+  //    }
+  //    if (game == null) {
+  //      return cb(new Error("Game not found."));
+  //    }
+  //    newsM.find({game: game._id}).populate("author").exec(function (err, news) {
+  //      if (err != null) {
+  //        return cb(err);
+  //      }
+  //      var kills = _.sortByOrder(game.kills, ["entryDate"], ["desc"]);
+  //      if (typeof data === "number" && data > 0) {
+  //        kills = _.slice(kills, 0, data);
+  //      }
+  //      cb(null, {
+  //        kills: _.map(kills, function (k) {
+  //          return _.extend(_.omit(k._doc, ["murderer", "victim"]), {
+  //            murderer: k.murderer ? _.pick(k.murderer._doc, ["_id", "username"]) : null,
+  //            victim: _.pick(k.victim._doc, ["_id", "username"])
+  //          });
+  //        }),
+  //        special: _.map(news, function (n) {
+  //          return _.extend(_.omit(n._doc, ["author"]), {author: _.pick(n.author._doc, ["_id", "username"])});
+  //        })
+  //      });
+  //    });
+  //  });
+  //});
 };
