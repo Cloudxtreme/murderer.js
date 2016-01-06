0a370cf7918f9375f966ecd199bde305f5f6fbdf
frissdiegurke <frissdiegurke@hotmail.com>
2016-01-06 08:52:00 +0100

    Fix murder socket routes resolve values

--- src/controller/socketRoutes/closed/murder.js
+++ src/controller/socketRoutes/closed/murder.js
@@ -1,5 +1,6 @@
 "use strict";
 
+var _ = require("lodash");
 var Q = require("q");
 var gameC = require.main.require("./core/game/controller");
 var security = require.main.require("./utils/security");
@@ -10,13 +11,14 @@ module.exports = function (queryRoute) {
       return Q.reject("Bad request.");
     }
     if (!security.checkPassword(data.password, this.user.pw)) { return Q.reject("Invalid password."); }
-    return gameC.qSuicide(this, this.user._id, data.gameId, data.message);
+    return gameC.qSuicide(this, this.user._id, data.gameId, data.message).then(_.noop);
   });
 
   queryRoute("murder:token", function (data) {
     if (typeof data !== "object" || data == null || typeof data.token !== "string" || typeof data.message !== "string" || typeof data.gameId !== "string" || !data.message.length) {
       return Q.reject("Bad request.");
     }
-    return gameC.qKillByToken(this, this.user._id, data.gameId, data.token, data.message);
+    return gameC.qKillByToken(this, this.user._id, data.gameId, data.token, data.message).then(_.noop);
+    // TODO return new contract instead of undefined
   });
 };
