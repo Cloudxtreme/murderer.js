bfa92500872d28f9b6b55616cef509b4c1d420ba
frissdiegurke <frissdiegurke@hotmail.com>
2016-01-13 14:47:33 +0100

    Remove relics from non-admin modules
    To tidy-up the not yet reworked code is removed.
    
    Refers #27

--- public/closed/scripts/controller/contractsCtrl.js
+++ public/closed/scripts/controller/contractsCtrl.js
@@ -18,50 +18,6 @@ angular.module("closed").controller("contractsCtrl", function ($scope, $rootScop
 
   /*=============================================== Initial Execution  ===============================================*/
 
-  socket.query("contracts:active").then(function (games) {
-    $scope.allGames = games;
-    /* Prepend '/' for layout development
-    var playerNameSelf = "frissdiegurke", groupName = "admins", playerName = "asterix", message = "Kill me please!";
-    var gameName1 = "Halloween Massacre", gameName2 = "Game #8128";
-    $scope.allGames = [
-      {
-        _id: "game-1",
-        name: gameName1,
-        multiGroup: true,
-        active: false,
-        alias: {name: playerNameSelf, group: {name: groupName}},
-        schedule: {end: "2016-01-21T23:00:00.000Z"},
-        contracts: [
-          {present: false},
-          {present: false, resolved: true, survivor: {name: playerName, message: message}},
-          {present: false, resolved: true, survivor: null},
-          {present: true, resolved: true, survived: true},
-          {present: true, resolved: true, survivor: null},
-          {present: true, resolved: true, survivor: {name: playerName, message: message}},
-          {present: true, dead: true},
-          {present: true, dead: false, token: "DEADBEEF", target: {name: playerName, message: message}}
-        ]
-      },
-      {
-        _id: "game-2",
-        name: gameName2,
-        multiGroup: false,
-        active: true,
-        alias: {name: playerNameSelf, group: {name: groupName}},
-        schedule: {end: "2016-01-28T23:00:00.000Z"},
-        contracts: _.shuffle([
-          {present: false},
-          {present: false, resolved: true, survivor: {name: playerName, message: message}},
-          {present: false, resolved: true, survivor: null},
-          {present: true, resolved: true, survived: true},
-          {present: true, resolved: true, survivor: null},
-          {present: true, resolved: true, survivor: {name: playerName, message: message}},
-          {present: true, dead: true},
-          {present: true, dead: false, token: "B105F00D", target: {name: playerName, message: message}}
-        ])
-      }
-    ];
-    //*/
-  });
+  socket.query("contracts:active").then(function (games) { $scope.allGames = games; });
 
 });
